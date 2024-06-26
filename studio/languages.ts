0a25dc1ca8348f190f61c1139768b0bf168af8b2
Tobias Wulvik <tobias.wulvik@gmail.com>
2024-06-26 19:31:25 +0200

    restart-june-24

--- studio/languages.ts
+++
@@ -1,9 +0,0 @@
-export const supportedLanguages = [
-  {id: 'no', title: 'Norwegian', isDefault: true},
-  {id: 'en', title: 'English'},
-]
-
-export type LocaleStringType = {no: string; en: string}
-export type CurrentLanguage = 'no' | 'en'
-
-export const baseLanguage = supportedLanguages.find((l) => l.isDefault)
